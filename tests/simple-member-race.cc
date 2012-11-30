d108fdb21b365ca9f129e4bdfa82e2b947061af2
Jing Zhang <jing@linux-crsq.(none)>
2012-11-30 15:15:17 -0500

    tests: P1

---
+++ tests/simple-member-race.cc
@@ -0,0 +1,50 @@
+#include <pthread.h>
+
+void * call_back1(void* A_ptr);
+void * call_back2(void* A_ptr);
+
+class A {
+    int dummy;
+    int member;
+    int xyz;
+
+  public:
+    void * __attribute__((annotate("self-write"))) Thread1(void *x) {
+        int abc = member;
+        return NULL;
+    }
+    void *__attribute__((annotate("self-write")))  Thread2(void *x) {
+        member=1;
+        return NULL;
+    }
+
+    friend void* call_back1(void* A_ptr);
+    friend void* call_back2(void* A_ptr);
+
+    void create_thread1(pthread_t* t) {
+        pthread_create(t, NULL, call_back1, this);
+    }
+    void create_thread2(pthread_t* t) {
+        pthread_create(t, NULL, call_back2, this);
+    }
+};
+
+void * call_back1(void* A_ptr) {
+    static_cast<A*>(A_ptr)->Thread1(NULL);
+    return NULL;
+}
+
+void * call_back2(void* A_ptr) {
+    static_cast<A*>(A_ptr)->Thread2(NULL);
+    return NULL;
+}
+
+int main() {
+    pthread_t t[2];
+    A aa;
+    aa.create_thread1(&t[0]);
+    aa.create_thread2(&t[1]);
+    pthread_join(t[0], NULL);
+    pthread_join(t[1], NULL);
+}
+
